0a9a83094f6917fd7105b1d34a41ccdbc4c07779
ajitrajasekharan <37787190+ajitrajasekharan@users.noreply.github.com>
2019-06-04 22:35:00 -0400

    Update second.sh

--- second.sh
+++ second.sh
@@ -1,4 +1,4 @@
 echo "Install Nividia drivers now"
 echo "Link: https://cloud.google.com/compute/docs/gpus/add-gpus#install-gpu-driver"
-#Use the script below only for Ubuntu 16.01
+#Use the script below only for Ubuntu 16.04
 #sudo ./ubuntu_1601_nvidia_driver.sh
