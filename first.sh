46ce9efd47fc9d21962af7a9726e67c853521bfb
ajitrajasekharan <ajitrajasekharan@icloud.com>
2019-06-06 16:11:02 -0400

    Minor fixes

--- first.sh
+++ first.sh
@@ -1,4 +1,6 @@
-echo "Google Cloud setup. Bootstrap step"
+echo "Google Cloud setup. Bootstrap step."
+sudo apt-get install screen
 sudo apt-get install zip
 sudo apt-get install bzip2
 sudo apt-get install vim
+echo "Launch screen -S <username> to continue work so we dont lose it from connection loss"
