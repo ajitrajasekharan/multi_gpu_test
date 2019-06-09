5cb3bf5548750c7ccba10d00a6a17f6c08598bf0
ajitrajasekharan <ajit@instance-1.c.ajit-compute.internal>
2019-06-09 21:16:05 +0000

    big fix

--- third.sh
+++ third.sh
@@ -2,8 +2,8 @@ sudo apt-get install zip
 wget https://repo.continuum.io/archive/Anaconda2-2018.12-Linux-x86_64.sh
 bash Anaconda2-2018.12-Linux-x86_64.sh
 echo "Starting PyTorch install"
-sleep 5
-(. ~/.bashrc; conda create -n bert python=3.5; conda activate bert; conda install PyTorch -c PyTorch; sudo apt install python3-pip; pip3 install torchvision; pip3 install pytorch-pretrained-bert; pip3 install seqeval; pip3 install tqdm; pip3 install nltk)
+sleep 2
+(set -a; . ~/.bashrc; set +a; which conda; sleep 5;  conda create -n bert python=3.5; conda activate bert; conda install PyTorch -c PyTorch; sudo apt install python3-pip; pip3 install torchvision; pip3 install pytorch-pretrained-bert; pip3 install seqeval; pip3 install tqdm; pip3 install nltk)
 
 echo "Installation complete"
 echo "Perform the following steps to activate environment"
