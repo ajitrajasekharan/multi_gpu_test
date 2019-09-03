06db16f573113cba78b53bf96158e7224595bc9a
ajitrajasekharan <ajit@instance-2.c.ajit-compute.internal>
2019-09-03 21:30:19 +0000

    changed to pytorch transfomers.

--- third.sh
+++ third.sh
@@ -1,9 +1,10 @@
-sudo apt-get install zip 
-wget https://repo.continuum.io/archive/Anaconda2-2018.12-Linux-x86_64.sh
-bash Anaconda2-2018.12-Linux-x86_64.sh
+#sudo apt-get install zip 
+#wget https://repo.continuum.io/archive/Anaconda2-2018.12-Linux-x86_64.sh
+#bash Anaconda2-2018.12-Linux-x86_64.sh
+#exit
 echo "Starting PyTorch install"
 sleep 2
-(set -a; . ~/.bashrc; set +a; which conda; sleep 5;  conda create -n bert python=3.5; conda activate bert; conda install PyTorch -c PyTorch; sudo apt install python3-pip; pip3 install torchvision; pip3 install pytorch-pretrained-bert; pip3 install seqeval; pip3 install tqdm; pip3 install nltk)
+(set -a; . ~/.bashrc; set +a; which conda; sleep 5;  conda create -n bert python=3.6; conda activate bert; conda install PyTorch -c PyTorch; sudo apt install python3-pip; pip3 install torchvision; pip install pytorch-transformers; pip3 install seqeval; pip3 install tqdm; pip3 install nltk)
 
 echo "Installation complete"
 echo "Perform the following steps to activate environment"
