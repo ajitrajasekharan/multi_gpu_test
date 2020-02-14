6bc1222444a005b28099acf707e149dc19af9041
ajitrajasekharan <ajitrajasekharan@icloud.com>
2020-02-14 16:26:39 +0000

    removed 10. version spec in nvidia driver update

--- ubuntu_16_04_nvidia_driver.sh
+++ ubuntu_16_04_nvidia_driver.sh
@@ -7,7 +7,8 @@ if ! dpkg-query -W cuda-10-0; then
   dpkg -i ./cuda-repo-ubuntu1604_10.0.130-1_amd64.deb
   apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/7fa2af80.pub
   apt-get update
-  apt-get install cuda-10-0 -y
+  #apt-get install cuda-10-0 -y
+  apt-get install cuda
 fi
 # Enable persistence mode 
 nvidia-smi -pm 1
