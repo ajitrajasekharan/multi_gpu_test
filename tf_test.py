02cdca48b8c3af3a63b177142bb18a6a8e21f85d
ajitrajasekharan <ajitrajasekharan@icloud.com>
2021-03-05 15:01:28 +0000

    initial add

---
+++ tf_test.py
@@ -0,0 +1,19 @@
+import tensorflow as tf
+
+c = []
+for i, d in enumerate(['/gpu:0', '/gpu:1', '/gpu:2','/gpu:3']):
+    with tf.device(d):
+        a = tf.get_variable(f"a_{i}", [2, 3], initializer=tf.random_uniform_initializer(-1, 1))
+        b = tf.get_variable(f"b_{i}", [3, 2], initializer=tf.random_uniform_initializer(-1, 1))
+        for i in range(10000):
+                c.append(tf.matmul(a, b))
+
+with tf.device('/cpu:0'):
+    sum = tf.add_n(c)
+
+sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
+
+init = tf.global_variables_initializer()
+sess.run(init)
+
+print(sess.run(sum))
